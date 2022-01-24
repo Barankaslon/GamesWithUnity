5a8b677d7e40cf596c6b24760da1c704ef83d20d
Alexey Gvozdev <72240384+Barankaslon@users.noreply.github.com>
2022-01-24 14:03:55 -0400

    obstacles positioning

---
+++ RoadRunner/Assets/Scripts/Obstacles Scripts/ObstacleHolder.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleHolder : MonoBehaviour
+{
+    public GameObject[] childs;
+    public float limitAxisX;
+    public Vector3 firstPos, secondPos;
+
+    void OnEnable()
+    {
+        
+    }
+}
